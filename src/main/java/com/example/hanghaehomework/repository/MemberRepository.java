f346789e6bd6da8f03ab107541913aecf301d493
Joyanggi <jyg8033@gmail.com>
2023-04-20 15:30:56 +0900

    Modified

--- src/main/java/com/example/hanghaehomework/repository/MemberRepository.java
+++ src/main/java/com/example/hanghaehomework/repository/MemberRepository.java
@@ -2,5 +2,8 @@ package com.example.hanghaehomework.repository;
 import com.example.hanghaehomework.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByUserId(String userId);
 }
--- src/main/java/com/example/hanghaehomework/service/MemberService.java
+++ src/main/java/com/example/hanghaehomework/service/MemberService.java
@@ -1,9 +1,13 @@
 package com.example.hanghaehomework.service;
 
 import com.example.hanghaehomework.dto.MemberRequestDto;
+import com.example.hanghaehomework.entity.Member;
+import com.example.hanghaehomework.jwt.JwtUtil;
 import com.example.hanghaehomework.repository.MemberRepository;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -12,7 +16,22 @@ public class MemberService {
     private final MemberRepository memberRepository;
     private final JwtUtil jwtUtil;
 
-    public MemberRequestDto signup() {
 
-    }
+
+    @Transactional
+        public void login(MemberRequestDto memberRequestDto, HttpServletRequest response){
+            String userName = memberRequestDto.getUsername();
+            String password = memberRequestDto.getPassword();
+
+            Member member = memberRepository.findByUserId(userName).orElseThrow(
+                    () -> new IllegalArgumentException("등록된 아이디가 없습니다.")
+            );
+
+            if(!member.getPassword().equals(password)){
+                throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+            }
+            response.addHeader(JwtUtil.AUTHORIZATION_HEADER, jwtUtil.createToken(member.getUsername())));
+        }
+
+
 }
