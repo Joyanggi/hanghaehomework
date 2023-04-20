5762cf83a3da71395f3634840c96d7c26dcd6979
Joyanggi <jyg8033@gmail.com>
2023-04-20 17:42:56 +0900

    Bug Fixed

--- src/main/java/com/example/hanghaehomework/controller/MemberController.java
+++ src/main/java/com/example/hanghaehomework/controller/MemberController.java
@@ -1,11 +1,14 @@
 package com.example.hanghaehomework.controller;
 
 import com.example.hanghaehomework.dto.MemberRequestDto;
+import com.example.hanghaehomework.dto.SignupRequestDto;
 import com.example.hanghaehomework.service.MemberService;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,4 +22,11 @@ public class MemberController {
         memberService.login(memberRequestDto, response);
         return "로그인 성공";
     }
+
+    @PostMapping("/api/auth/signup")
+    public ResponseEntity<String> signup(@RequestBody SignupRequestDto signupRequestDto) {
+        memberService.signup(signupRequestDto);
+        return new ResponseEntity<>("회원가입 성공", HttpStatus.CREATED);
+    }
+
 }
--- src/main/java/com/example/hanghaehomework/service/MemberService.java
+++ src/main/java/com/example/hanghaehomework/service/MemberService.java
@@ -1,6 +1,7 @@
 package com.example.hanghaehomework.service;
 
 import com.example.hanghaehomework.dto.MemberRequestDto;
+import com.example.hanghaehomework.dto.SignupRequestDto;
 import com.example.hanghaehomework.entity.Member;
 import com.example.hanghaehomework.jwt.JwtUtil;
 import com.example.hanghaehomework.repository.MemberRepository;
@@ -9,6 +10,8 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -28,11 +31,20 @@ public class MemberService {
                     () -> new IllegalArgumentException("등록된 아이디가 없습니다.")
             );
 
-            if(!member.getPassword().equals(password)){
-                throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
-            }
-            response.addHeader(JwtUtil.AUTHORIZATION_HEADER, jwtUtil.createToken(member.getUsername()));
+        if(!member.getPassword().equals(password)){
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
         }
+        response.addHeader(JwtUtil.AUTHORIZATION_HEADER, jwtUtil.createToken(member.getUsername()));
+    }
+    @Transactional
+    public void signup(SignupRequestDto signupRequestDto)  {
+        //회원가입 유저 아이디 중복확인
+        Optional<Member> overlapUser = memberRepository.findByUserId(signupRequestDto.getUsername());
+        if(overlapUser.isPresent()) throw new IllegalArgumentException("중복된 아이디가 있습니다.");
+        memberRepository.save(new Member(signupRequestDto));
+
+    }
+
 
 
 }
